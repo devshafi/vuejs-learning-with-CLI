f0c53a845f51fdb00e310ec459c32bf3842676c3
frshafi49 <frshafi49@gmail.com>
2019-01-27 19:01:00 +0600

    added body of the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <app-header></app-header>
-
-    
-
+    <app-ninjas></app-ninjas>
     <app-footer></app-footer>
   </div>
 </template>
@@ -14,13 +12,15 @@
 // registering components locally
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 
 
 export default {
   
   components:{
     'app-header': Header,
-    'app-footer': Footer
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
   },
 
   data(){
